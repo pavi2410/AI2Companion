787d4d91e83f6215b8a39144a56dc30a77df9656
Pavitra Golchha <hello@pavi2410.me>
2022-02-11 16:27:49 +0530

    Switch to Gradle Kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "AI2Companion"
+
+include(":app")
+include(":barcode")
